c10271038b876e5585d8e3d8ff61ab072a3ba441
marco <enidev911@gmail.com>
2023-05-19 01:02:43 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'pages', to: 'pages#index'
+  root 'pages#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
